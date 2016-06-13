947dac499b128e00ecf9f7474fc26f0978cfb8fc
Thoba Lose <thobalose@users.noreply.github.com>
2016-06-13 15:38:51 +0200

    Update set_neo4j_settings.sh

--- set_neo4j_settings.sh
+++ set_neo4j_settings.sh
@@ -11,10 +11,6 @@ setting() {
 
 cd ${NEO4J_INSTALL_DIR:=/opt/neo4j}
 
-if [ "${NEO4J_AUTH:-}" == "none" ]; then
-    setting "dbms.security.auth_enabled" "false" neo4j-server.properties
-fi
-
 setting "keep_logical_logs" "${NEO4J_KEEP_LOGICAL_LOGS:-100M size}" neo4j.properties
 setting "dbms.pagecache.memory" "${NEO4J_CACHE_MEMORY:-512M}" neo4j.properties
 setting "wrapper.java.additional=-Dneo4j.ext.udc.source" "${NEO4J_UDC_SOURCE:-docker}" neo4j-wrapper.conf
@@ -22,6 +18,10 @@ setting "wrapper.java.initmemory" "${NEO4J_HEAP_MEMORY:-512}" neo4j-wrapper.conf
 setting "wrapper.java.maxmemory" "${NEO4J_HEAP_MEMORY:-512}" neo4j-wrapper.conf
 setting "org.neo4j.server.thirdparty_jaxrs_classes" "${NEO4J_THIRDPARTY_JAXRS_CLASSES:-}" neo4j-server.properties
 setting "allow_store_upgrade" "${NEO4J_ALLOW_STORE_UPGRADE:-}" neo4j.properties
+
+if [ "${NEO4J_AUTH:-}" == "none" ]; then
+    setting "dbms.security.auth_enabled" "false" neo4j-server.properties
+fi
 
 setting "org.neo4j.server.webserver.address" "0.0.0.0" neo4j-server.properties
 setting "org.neo4j.server.database.mode" "${NEO4J_DATABASE_MODE:-}" neo4j-server.properties
