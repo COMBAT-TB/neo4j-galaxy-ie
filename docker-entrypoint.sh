4c276be0c181292bbf7e560b1f8767dea109c550
Peter van Heusden <pvh@sanbi.ac.za>
2017-06-13 11:41:10 +0200

    Replaced NEO4J_ vars with USER_ ones

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,34 +4,34 @@ NEO4JDB_PATH=/data/neo4jdb
 export NEO4JDB_PATH
 
 if [ "$1" == "neo4j" ]; then
-    if [ "${NEO4J_UID:=none}" = "none" -o "${NEO4J_GID:=none}" = "none" ] ; then
-        echo "You need to set the NEO4J_UID and NEO4J_GID environment variables to use this container." >&2
+    if [ "${USER_UID:=none}" = "none" -o "${USER_GID:=none}" = "none" ] ; then
+        echo "You need to set the USER_UID and USER_GID environment variables to use this container." >&2
         exit 1
     fi
 
-    NEO4J_GROUP=$(getent group $NEO4J_GID | cut -d: -f1)
-    if [ -z "$NEO4J_GROUP" ] ; then
-        NEO4J_GROUP=neo4j
-        groupadd -g $NEO4J_GID $NEO4J_GROUP
-        echo "Added $NEO4J_GID to $NEO4J_GROUP"
+    USER_GROUP=$(getent group $USER_GID | cut -d: -f1)
+    if [ -z "$USER_GROUP" ] ; then
+        USER_GROUP=neo4j
+        groupadd -g $USER_GID $USER_GROUP
+        echo "Added $USER_GID to $USER_GROUP"
     fi
-    NEO4J_USER=$(getent passwd $NEO4J_UID | cut -d: -f1)
-    if [ -z "$NEO4J_USER" ] ; then
-        NEO4J_USER=neo4j
-        useradd -u $NEO4J_UID -g $NEO4J_GROUP $NEO4J_USER
+    USER_USER=$(getent passwd $USER_UID | cut -d: -f1)
+    if [ -z "$USER_USER" ] ; then
+        USER_USER=neo4j
+        useradd -u $USER_UID -g $USER_GROUP $USER_USER
     fi
-    chown -R $NEO4J_UID:$NEO4J_GID /opt /data
+    chown -R $USER_UID:$USER_GID /opt /data
     EXISTING_UID=$(stat -c '%u' /data)
     EXISTING_GID=$(stat -c '%g' /data)
     echo "/opt /data $EXISTING_UID $EXISTING_GID"
-    if [ $(stat -c '%u' /data) -ne $NEO4J_UID -o $(stat -c '%g' /data) -ne $NEO4J_GID ] ; then
+    if [ $(stat -c '%u' /data) -ne $USER_UID -o $(stat -c '%g' /data) -ne $USER_GID ] ; then
         EXISTING_UID=$(stat -c '%u' /data)
         EXISTING_GID=$(stat -c '%g' /data)
-        echo "The /data volume must be owned by user ID $NEO4J_UID and group ID $NEO4J_GID, instead it is owned by ${EXISTING_UID}: ${EXISTING_GID}" >&2
+        echo "The /data volume must be owned by user ID $USER_UID and group ID $USER_GID, instead it is owned by ${EXISTING_UID}: ${EXISTING_GID}" >&2
         exit 1
     fi
     if [ ! -d $NEO4JDB_PATH ] ; then
-        gosu $NEO4J_UID:$NEO4J_GID cp -r /opt/neo4j/data $NEO4JDB_PATH
+        gosu $USER_UID:$USER_GID cp -r /opt/neo4j/data $NEO4JDB_PATH
         echo "Initialising new database in $NEO4JDB_PATH"
         # echo "There is no database in $NEO4JDB_PATH, will exit." >&2
         # exit 1
@@ -47,7 +47,7 @@ if [ "$1" == "neo4j" ]; then
     # connection so this is safe
     /monitor_traffic.sh &
 
-    gosu $NEO4J_UID:$NEO4J_GID /run_neo4j.sh
+    gosu $USER_UID:$USER_GID /run_neo4j.sh
 elif [ "$1" == "dump-config" ]; then
     if [ -d /conf ]; then
         cp --recursive conf/* /conf
